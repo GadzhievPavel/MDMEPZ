15f3a983bc6c13cdd5f0e45df7db20841153b6a6
Maryushkin Anton <62674062+maikhun@users.noreply.github.com>
2023-09-05 14:38:36 +0300

    Add files via upload

--- MDMEPZ/Model/References1C/CategoryProduct/CategoryProductReference.cs
+++ MDMEPZ/Model/References1C/CategoryProduct/CategoryProductReference.cs
@@ -1,1 +1,19 @@
-namespace TFlex.DOCs.References.CategoryProduct{	using System;	using TFlex.DOCs.Model.References;	using TFlex.DOCs.Model.Structure;	using TFlex.DOCs.Model.Classes;	using TFlex.DOCs.Model;			public partial class CategoryProductReference : SpecialReference<CategoryProductReferenceObject>	{				public partial class Factory		{		}	}}
+namespace TFlex.DOCs.References.CategoryProduct{	using System;	using TFlex.DOCs.Model.References;	using TFlex.DOCs.Model.Structure;	using TFlex.DOCs.Model.Classes;	using TFlex.DOCs.Model;
+    using System.Runtime.Remoting.Contexts;
+    using System.Collections.Generic;
+    using MDMEPZ.Dto;
+    using TFlex.DOCs.References.GroupList;
+    using MDMEPZ.Util;
+
+    public partial class CategoryProductReference : SpecialReference<CategoryProductReferenceObject>	{				public partial class Factory		{		}
+        
+        public ReferenceObject CreateReferenceObject(ProductCategory productCategory)
+        {
+            var o = CreateReferenceObject() as CategoryProductReferenceObject;
+            o.StartUpdate();
+            o.Name.Value = productCategory.Name;
+            o.GUID_1C.Value = new Guid(productCategory.Guid1C);
+            o.EndChanges();
+            return o;
+        }
+    }}
--- MDMEPZ/Model/References1C/CategoryProduct/CategoryProductReferenceObject.cs
+++ MDMEPZ/Model/References1C/CategoryProduct/CategoryProductReferenceObject.cs
@@ -1,1 +1,23 @@
-namespace TFlex.DOCs.References.CategoryProduct{	using System;	using TFlex.DOCs.Model.References;	using TFlex.DOCs.Model.Structure;	using TFlex.DOCs.Model.References.Links;	using TFlex.DOCs.Model.Classes;	using TFlex.DOCs.Model.Parameters;			public partial class CategoryProductReferenceObject : SpecialReferenceObject<CategoryProductReference>	{	}}
+namespace TFlex.DOCs.References.CategoryProduct{	using System;	using TFlex.DOCs.Model.References;	using TFlex.DOCs.Model.Structure;	using TFlex.DOCs.Model.References.Links;	using TFlex.DOCs.Model.Classes;	using TFlex.DOCs.Model.Parameters;
+    using TFlex.DOCs.Model.Plugins;
+    using System.Collections.Generic;
+    using TFlex.DOCs.Model.Desktop;
+    using TFlex.DOCs.Model.References.Units;
+    using TFlex.DOCs.References.UnitOfMeasurement;
+    using MDMEPZ.Dto;
+
+    public partial class CategoryProductReferenceObject : SpecialReferenceObject<CategoryProductReference>	{		public String CreateCategoryProductReferenceObject(ProductCategory[] categoryProducts)
+		{
+            CategoryProductReference categoryProductReference;
+			List<ReferenceObject> saveList = new List<ReferenceObject>();
+            var c = categoryProductReference.CreateReferenceObject();
+			c.EndChanges();
+            foreach (ProductCategory category in categoryProducts)
+            {
+                var obj = categoryProductReference.CreateReferenceObject(category);
+                obj.EndChanges();
+                saveList.Add(obj);
+            }
+            Desktop.CheckIn(saveList, "", false);
+            return "correctly";
+        }	}}

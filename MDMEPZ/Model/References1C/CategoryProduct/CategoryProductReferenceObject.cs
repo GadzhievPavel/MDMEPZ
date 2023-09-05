24ff057ffddeeceb4584697a7dff2b71fc01dc6a
Maryushkin Anton <62674062+maikhun@users.noreply.github.com>
2023-09-05 14:41:57 +0300

    Update CategoryProductReferenceObject.cs

--- MDMEPZ/Model/References1C/CategoryProduct/CategoryProductReferenceObject.cs
+++ MDMEPZ/Model/References1C/CategoryProduct/CategoryProductReferenceObject.cs
@@ -1,4 +1,11 @@
-namespace TFlex.DOCs.References.CategoryProduct{	using System;	using TFlex.DOCs.Model.References;	using TFlex.DOCs.Model.Structure;	using TFlex.DOCs.Model.References.Links;	using TFlex.DOCs.Model.Classes;	using TFlex.DOCs.Model.Parameters;
+namespace TFlex.DOCs.References.CategoryProduct
+{
+	using System;
+	using TFlex.DOCs.Model.References;
+	using TFlex.DOCs.Model.Structure;
+	using TFlex.DOCs.Model.References.Links;
+	using TFlex.DOCs.Model.Classes;
+	using TFlex.DOCs.Model.Parameters;
     using TFlex.DOCs.Model.Plugins;
     using System.Collections.Generic;
     using TFlex.DOCs.Model.Desktop;
@@ -6,18 +13,7 @@ namespace TFlex.DOCs.References.CategoryProduct{	using System;	using TFlex.DO
     using TFlex.DOCs.References.UnitOfMeasurement;
     using MDMEPZ.Dto;
 
-    public partial class CategoryProductReferenceObject : SpecialReferenceObject<CategoryProductReference>	{		public String CreateCategoryProductReferenceObject(ProductCategory[] categoryProducts)
-		{
-            CategoryProductReference categoryProductReference;
-			List<ReferenceObject> saveList = new List<ReferenceObject>();
-            var c = categoryProductReference.CreateReferenceObject();
-			c.EndChanges();
-            foreach (ProductCategory category in categoryProducts)
-            {
-                var obj = categoryProductReference.CreateReferenceObject(category);
-                obj.EndChanges();
-                saveList.Add(obj);
-            }
-            Desktop.CheckIn(saveList, "", false);
-            return "correctly";
-        }	}}
+    public partial class CategoryProductReferenceObject : SpecialReferenceObject<CategoryProductReference>
+	{
+	}
+}

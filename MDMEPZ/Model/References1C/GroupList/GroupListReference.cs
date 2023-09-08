91d2813a7594117e66108f0a25496e5fafda834d
Maryushkin Anton <62674062+maikhun@users.noreply.github.com>
2023-09-08 12:03:28 +0300

    Update GroupListReference.cs

--- MDMEPZ/Model/References1C/GroupList/GroupListReference.cs
+++ MDMEPZ/Model/References1C/GroupList/GroupListReference.cs
@@ -1,15 +1,30 @@
-namespace TFlex.DOCs.References.GroupList{	using System;	using TFlex.DOCs.Model.References;	using TFlex.DOCs.Model.Structure;	using TFlex.DOCs.Model.Classes;	using TFlex.DOCs.Model;
+namespace TFlex.DOCs.References.GroupList
+{
+	using System;
+	using TFlex.DOCs.Model.References;
+	using TFlex.DOCs.Model.Structure;
+	using TFlex.DOCs.Model.Classes;
+	using TFlex.DOCs.Model;
     using MDMEPZ.Dto;
     using MDMEPZ.Util;
     using TFlex.DOCs.Model.Parameters;
     using TFlex.DOCs.Model.Desktop;
 
-    public partial class GroupListReference : SpecialReference<GroupListReferenceObject>	{				public partial class Factory		{		}		public ReferenceObject CreateReferenceObject(GroupOfList groupOfList)
+    public partial class GroupListReference : SpecialReference<GroupListReferenceObject>
+	{
+		
+		public partial class Factory
+		{
+		}
+
+		public ReferenceObject CreateReferenceObject(GroupOfList groupOfList)
 		{
 			var o = CreateReferenceObject() as GroupListReferenceObject;
 			o.StartUpdate();
 			o.Name.Value = groupOfList.Name;
-			o.GUID_1C.Value = groupOfList.Guid1C;
+			o.GUID_1C.Value = new Guid(groupOfList.Guid1C);
 			o.EndChanges();
 			return o;
-		}	}}
+		}
+	}
+}

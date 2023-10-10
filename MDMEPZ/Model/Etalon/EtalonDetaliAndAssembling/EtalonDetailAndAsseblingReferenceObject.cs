1118af7e14dde0d85c3d80cf039bb2910e4c6c3f
iliSON47 <iburych@bk.ru>
2023-10-10 14:55:32 +0300

    1

--- MDMEPZ/Model/Etalon/EtalonDetaliAndAssembling/EtalonDetailAndAsseblingReferenceObject.cs
+++ MDMEPZ/Model/Etalon/EtalonDetaliAndAssembling/EtalonDetailAndAsseblingReferenceObject.cs
@@ -1,22 +1,11 @@
-namespace TFlex.DOCs.References.EtalonDetailAndAssebly
-{
-	using System;
-	using TFlex.DOCs.Model.References;
-	using TFlex.DOCs.Model.Structure;
-	using TFlex.DOCs.Model.References.Links;
-	using TFlex.DOCs.Model.Classes;
-	using TFlex.DOCs.Model.Parameters;
+namespace TFlex.DOCs.References.EtalonDetailAndAssebly{	using System;	using TFlex.DOCs.Model.References;	using TFlex.DOCs.Model.Structure;	using TFlex.DOCs.Model.References.Links;	using TFlex.DOCs.Model.Classes;	using TFlex.DOCs.Model.Parameters;
 	using TFlex.DOCs.Model.Macros.ObjectModel;
     using TFlex.DOCs.References.NomenclatureERP;
     using MDMEPZ.Util;
 	using TFlex.DOCs.Model.References.Nomenclature;
 	using TFlex.DOCs.Model.References.Documents;
 
-	public partial class EtalonDetailAndAsseblingReferenceObject : SpecialReferenceObject<EtalonDetailAndAsseblingReference>
-	{
-		private Reference refDocument;
-		private NomenclatureReference refNomenclature;
-		public ReferenceObject CreateInPDM()
+	public partial class EtalonDetailAndAsseblingReferenceObject : SpecialReferenceObject<EtalonDetailAndAsseblingReference>	{		private Reference refDocument;		private NomenclatureReference refNomenclature;		public ReferenceObject CreateInPDM()
 		{
 
             var nomenclatureERP = this.Nomenclature as NomenclatureERPReferenceObject;
@@ -25,7 +14,4 @@ namespace TFlex.DOCs.References.EtalonDetailAndAssebly
             nomenclatureERP.StartUpdate();
             nomenclatureERP.Nomenclature = objEsi;
             nomenclatureERP.EndUpdate("");
-        }
-	}
-}
-
+        }	}}

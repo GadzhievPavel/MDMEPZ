eee9256757de35953daddf2c69f17742564497cf
iliSON47 <iburych@bk.ru>
2023-10-10 14:55:42 +0300

    2

--- MDMEPZ/Model/Etalon/EtalonDetaliAndAssembling/EtalonDetailAndAsseblingReferenceObject.cs
+++ MDMEPZ/Model/Etalon/EtalonDetaliAndAssembling/EtalonDetailAndAsseblingReferenceObject.cs
@@ -1,17 +1,34 @@
-namespace TFlex.DOCs.References.EtalonDetailAndAssebly{	using System;	using TFlex.DOCs.Model.References;	using TFlex.DOCs.Model.Structure;	using TFlex.DOCs.Model.References.Links;	using TFlex.DOCs.Model.Classes;	using TFlex.DOCs.Model.Parameters;
+namespace TFlex.DOCs.References.EtalonDetailAndAssebly
+{
+	using System;
+	using TFlex.DOCs.Model.References;
+	using TFlex.DOCs.Model.Structure;
+	using TFlex.DOCs.Model.References.Links;
+	using TFlex.DOCs.Model.Classes;
+	using TFlex.DOCs.Model.Parameters;
 	using TFlex.DOCs.Model.Macros.ObjectModel;
     using TFlex.DOCs.References.NomenclatureERP;
     using MDMEPZ.Util;
 	using TFlex.DOCs.Model.References.Nomenclature;
 	using TFlex.DOCs.Model.References.Documents;
 
-	public partial class EtalonDetailAndAsseblingReferenceObject : SpecialReferenceObject<EtalonDetailAndAsseblingReference>	{		private Reference refDocument;		private NomenclatureReference refNomenclature;		public ReferenceObject CreateInPDM()
+	public partial class EtalonDetailAndAsseblingReferenceObject : SpecialReferenceObject<EtalonDetailAndAsseblingReference>
+	{
+		private Reference refDocument;
+		private NomenclatureReference refNomenclature;
+		public ReferenceObject CreateInPDM()
 		{
+			if (this.refDocument != null)
+			{
+                var nomenclatureERP = this.Nomenclature as NomenclatureERPReferenceObject;
+                var document = refDocument.CreateReferenceObject();
+                var objEsi = refNomenclature.CreateNomenclatureObject(document, null, null, null, false);
+                nomenclatureERP.StartUpdate();
+                nomenclatureERP.Nomenclature = objEsi;
+                nomenclatureERP.EndUpdate("");
+            }
+			return null;
+        }
+	}
+}
 
-            var nomenclatureERP = this.Nomenclature as NomenclatureERPReferenceObject;
-            var document = refDocument.CreateReferenceObject();
-            var objEsi = refNomenclature.CreateNomenclatureObject(document, null, null, null, false);
-            nomenclatureERP.StartUpdate();
-            nomenclatureERP.Nomenclature = objEsi;
-            nomenclatureERP.EndUpdate("");
-        }	}}

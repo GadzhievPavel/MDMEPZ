fc332f4c787bfef39272100217baef1a4b93142f
Павел Гаджиев <gadzhievpasha@gmail.com>
2023-10-30 16:56:26 +0300

    Refactoring replacements

--- MDMEPZ/Model/Etalon/EtalonStandartProduct/EtalonStandartProductReference.cs
+++ MDMEPZ/Model/Etalon/EtalonStandartProduct/EtalonStandartProductReference.cs
@@ -1,12 +1,27 @@
-namespace TFlex.DOCs.References.StandartProduct{	using System;	using TFlex.DOCs.Model.References;	using TFlex.DOCs.Model.Structure;	using TFlex.DOCs.Model.Classes;	using TFlex.DOCs.Model;
+namespace TFlex.DOCs.References.StandartProduct
+{
+	using System;
+	using TFlex.DOCs.Model.References;
+	using TFlex.DOCs.Model.Structure;
+	using TFlex.DOCs.Model.Classes;
+	using TFlex.DOCs.Model;
     using TFlex.DOCs.References.NomenclatureERP;
     using MDMEPZ.Util;
 
-    public partial class EtalonStandartProductReference : SpecialReference<EtalonStandartProductReferenceObject>	{				public partial class Factory		{		}		public ReferenceObject CreateReferenceObject(NomenclatureERPReferenceObject nomenclature)
+    public partial class EtalonStandartProductReference : SpecialReference<EtalonStandartProductReferenceObject>
+	{
+		
+		public partial class Factory
+		{
+		}
+
+		public ReferenceObject CreateReferenceObject(NomenclatureERPReferenceObject nomenclature)
 		{
 			var etalon = CreateReferenceObject() as EtalonStandartProductReferenceObject;
 			etalon.StartUpdate();
 			etalon.Name.Value = nomenclature.Name.Value;
 			etalon.Nomenclature = nomenclature;
 			return etalon;
-		}	}}
+		}
+	}
+}
---
+++ MDMEPZ/Model/StagesEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MDMEPZ.Model
+{
+    public class StagesMDM
+    {
+        public static readonly Guid filtering = new Guid("5c40376d-e2bf-4435-a8ec-277ed4f83424");
+        public static readonly Guid etalon = new Guid("f26404ef-7ff1-41d4-a841-dfb4bab1aac9");
+    }
+}

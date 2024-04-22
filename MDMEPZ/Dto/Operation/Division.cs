fa5a7ef397d98519b218cfb58e778fd1db5f8bdf
workPC <iburych@bk.ru>
2024-04-22 13:42:18 +0300

    Division
    add parameters

--- MDMEPZ/Dto/Operation/Division.cs
+++ MDMEPZ/Dto/Operation/Division.cs
@@ -7,5 +7,8 @@
     {
         public string TYPE { get; set; }
         public string UID { get; set; }
+        public string Наименование { get; set; }
+        public string Код { get; set; }
+     
     }
 }

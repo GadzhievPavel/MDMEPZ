a11d70f8d8f1ca07b5473d4e15c1ee8bf5dc91fa
workPC <iburych@bk.ru>
2024-04-25 14:28:56 +0300

    add DTO TypeOperation

---
+++ MDMEPZ/Model/References1C/TypeOperation/TypeOperationMDMReference.cs
@@ -0,0 +1,22 @@
+namespace TFlex.DOCs.References.TypeOperationMDM{
+    using MDMEPZ.Dto;
+    using MDMEPZ.Util;
+    using TFlex.DOCs.Model.References;
+
+    public partial class TypeOperationMDMReference : SpecialReference<TypeOperationMDMReferenceObject>
+    {
+
+        public partial class Factory
+        {
+        }
+        public ReferenceObject CreateReferenceObjectTypeOperation(TypeOperationMDM typeOperation)
+        {
+            var typeReferenceObject = CreateReferenceObject(Classes.MainTypeOperation) as TypeOperationMDMReferenceObject;
+            typeReferenceObject.StartUpdate();
+            typeReferenceObject.UID.Value = typeOperation.Ссылка.UID;
+            typeReferenceObject.Name.Value = typeOperation.Наименование;
+            typeReferenceObject.Kod.Value = typeOperation.Код;
+
+            return typeReferenceObject;
+        }
+    }}

d95151a1366fd5078eff13615517299c04ea13b9
workPC <iburych@bk.ru>
2024-05-03 14:21:30 +0300

    ADD: RankReference

---
+++ MDMEPZ/Model/References1C/Rank/RankReference.cs
@@ -0,0 +1,1 @@
+namespace TFlex.DOCs.References.Rank{	using System;	using TFlex.DOCs.Model.References;	using TFlex.DOCs.Model.Structure;	using TFlex.DOCs.Model.Classes;	using TFlex.DOCs.Model;			public partial class RankReference : SpecialReference<RankReferenceObject>	{				public partial class Factory		{		}	}}

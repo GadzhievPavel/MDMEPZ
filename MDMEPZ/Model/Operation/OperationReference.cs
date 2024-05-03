ad3fee2335c1f1b1df1475a0150e1429ef26ad9e
workPC <iburych@bk.ru>
2024-05-03 13:48:31 +0300

    Edit: CreateReferenceOperation)(

--- MDMEPZ/Model/Operation/OperationReference.cs
+++ MDMEPZ/Model/Operation/OperationReference.cs
@@ -1,4 +1,5 @@
-namespace TFlex.DOCs.References.Operation{
+namespace TFlex.DOCs.References.Operation
+{
     using MDMEPZ.Dto;
     using MDMEPZ.Util;
     using TFlex.DOCs.Model.References;
@@ -14,17 +15,19 @@ namespace TFlex.DOCs.References.Operation{
         /// Создание операции MDM с Определенным типом
         /// </summary>
         /// <param name="operation">Операция из .json</param>
-        /// <returns>Объект справочника Операции MDM</returns>        public ReferenceObject CreateReferenceObjectOperation(Operation operation)
+        /// <returns>Объект справочника Операции MDM</returns>
+        public ReferenceObject CreateReferenceObjectOperation(Operation operation)
         {
             var operationReferenceObject = CreateReferenceObject(Classes.AssemblyOperationType) as OperationReferenceObject;
             operationReferenceObject.StartUpdate();
-            operationReferenceObject.Name.Value = operation.Наименование;
+            operationReferenceObject.Name.Value = operation.Наименование.Remove(0,3);
             operationReferenceObject.Kod.Value = operation.Код;
-            operationReferenceObject.NomerOperatsii.Value = operation.НомерОперации;
-            operationReferenceObject.PoryadkovyyNomer.Value = operation.ПорядковыйНомер;
+            operationReferenceObject.NomerOperatsii.Value = operation.НомерОперации.Trim();
+            operationReferenceObject.PoryadkovyyNomer.Value = operation.ПорядковыйНомер.Trim(new char[]{' ','.'});
             operationReferenceObject.UID.Value = operation.Ссылка.UID;
             operationReferenceObject.Vladelets.Value = operation.Владелец.UID;
             operationReferenceObject.Zakhod_UID.Value = operation.Заход.UID;
             return operationReferenceObject;
         }
-    }}
+    }
+}

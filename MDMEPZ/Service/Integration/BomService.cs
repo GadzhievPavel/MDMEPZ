17d4b225e3b85bd36f83aa4747d220e3e596b8df
Павел Гаджиев <gadzhievpasha@gmail.com>
2024-05-15 15:34:34 +0300

    сериализация номенклатуры

---
+++ MDMEPZ/Service/Integration/BomService.cs
@@ -0,0 +1,49 @@
+﻿using MDMEPZ.Dto;
+using MDMEPZ.Util;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using TFlex.DOCs.Model.References.Nomenclature;
+using TFlex.DOCs.References.NomenclatureERP;
+
+namespace MDMEPZ.Service.Integration
+{
+    public class BomService
+    {
+        private NomenclatureObject nomenclature;
+        private HashSet<NomenclatureObject> nomenclatureObjects;
+        private At3bootSerializator serializator;
+        public BomService(NomenclatureObject rootNom, HashSet<NomenclatureObject> nomenclatures)
+        {
+            this.nomenclature = rootNom;
+            this.nomenclatureObjects = nomenclatures;
+            this.serializator = new At3bootSerializator();
+        }
+
+        public string GetBom(string path = null)
+        {
+            BomDto bomDto = new BomDto();
+
+            List<Nomenclature> nomListTemp = new List<Nomenclature>();
+            foreach (var nom in nomenclatureObjects)
+            {
+                var mdm = nom.GetObject(NomenclatureERPReferenceObject.RelationKeys.Nomenclature) as NomenclatureERPReferenceObject;
+                var nomDto = Nomenclature.CreateInstance(mdm);
+                nomListTemp.Add(nomDto);
+            }
+
+            bomDto.nomenclatures = nomListTemp.ToArray();
+
+            if (path != null)
+            {
+                return serializator.saveFile(bomDto, path);
+            }
+            else
+            {
+                return JsonConvert.SerializeObject(bomDto, Formatting.Indented);
+            }
+        }
+        
+    }
+}
---
+++ MDMEPZ/Util/At3bootSerializator.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MDMEPZ.Util
+{
+    public class At3bootSerializator
+    {
+        public string saveFile<T>(T dto, string path)
+        {
+            var str = JsonConvert.SerializeObject(dto, Formatting.Indented);
+
+            UTF8Encoding encoding = new UTF8Encoding();
+            using (FileStream fs = new FileStream(path, FileMode.Create))
+            {
+                var bytes = encoding.GetBytes(str);
+                fs.Write(bytes, 0, bytes.Length);
+            }
+
+            return str;
+        }
+    }
+}

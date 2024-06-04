45cf0748a2a40e6b17e4bd09f7227fb1818d632c
workPC <iburych@bk.ru>
2024-06-04 16:26:37 +0300

    Добавлена проверка типа объектов на входе от которых зависит тип создаваемой операции

--- MDMEPZ/Model/Operation/OperationReference.cs
+++ MDMEPZ/Model/Operation/OperationReference.cs
@@ -2,8 +2,11 @@ namespace TFlex.DOCs.References.Operation
 {
     using MDMEPZ.Dto;
     using MDMEPZ.Util;
+    using System.Linq;
     using TFlex.DOCs.Model.References;
-
+    using TFlex.DOCs.Model.References.Nomenclature;
+    using TFlex.DOCs.Model.Search;
+    using TFlex.DOCs.References.NomenclatureERP;
 
     public partial class OperationReference : SpecialReference<OperationReferenceObject>
     {
@@ -18,12 +21,41 @@ namespace TFlex.DOCs.References.Operation
         /// <returns>Объект справочника Операции MDM</returns>
         public ReferenceObject CreateReferenceObjectOperation(Operation operation)
         {
-            var operationReferenceObject = CreateReferenceObject(Classes.AssemblyOperationType) as OperationReferenceObject;
+
+            OperationReferenceObject operationReferenceObject = null;
+            var referenceNumenclatureERP = Connection.ReferenceCatalog.Find(3496).CreateReference();//справочник Номенклатуры ERP
+
+            bool flag = false;// false значит у объекта на входе не сборка
+            var listRows = operation.ОсновныеВходы.ROWS;
+            foreach (var row in listRows)
+            {
+                var result = referenceNumenclatureERP.Find(Filter.Parse($"[GUID(1C)] = '{row.Заход.UID}'", referenceNumenclatureERP.ParameterGroup)).First();
+                if (result != null)
+                {
+                    var objESI = result.GetObject(NomenclatureERPReferenceObject.RelationKeys.Nomenclature);
+                    if (objESI != null && (objESI.Class.Guid.Equals(NomenclatureTypes.Keys.Assembly.ToString()) || objESI.Class.Guid.Equals(NomenclatureTypes.Keys.AssemblyNode)))
+                    {
+                        flag = true;
+                    }
+                }
+
+            }
+
+            if (flag)
+            {
+                operationReferenceObject = CreateReferenceObject(Classes.AssemblyOperationType) as OperationReferenceObject;
+            }
+            else
+            {
+                operationReferenceObject = CreateReferenceObject(Classes.DefaultOperationType) as OperationReferenceObject;
+            }
+
+
             operationReferenceObject.StartUpdate();
-            operationReferenceObject.Name.Value = operation.Наименование.Remove(0,4);
+            operationReferenceObject.Name.Value = operation.Наименование.Remove(0, 4);
             operationReferenceObject.Kod.Value = operation.Код;
             operationReferenceObject.NomerOperatsii.Value = operation.НомерОперации.Trim();
-            operationReferenceObject.PoryadkovyyNomer.Value = operation.ПорядковыйНомер.Trim(new char[]{' ','.'});
+            operationReferenceObject.PoryadkovyyNomer.Value = operation.ПорядковыйНомер.Trim(new char[] { ' ', '.' });
             operationReferenceObject.UID.Value = operation.Ссылка.UID;
             operationReferenceObject.Vladelets.Value = operation.Владелец.UID;
             operationReferenceObject.Zakhod_UID.Value = operation.Заход.UID;

726ab5854732d93db84bf103c85cb0a639f50308
workPC <iburych@bk.ru>
2024-06-14 11:07:42 +0300

    Add DTO заход

---
+++ MDMEPZ/Dto/Route/Visit/LinkVisit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MDMEPZ.Dto
+{
+  public  class LinkVisit
+    {
+        public string TYPE { get; set; }
+        public string UID { get; set; }
+
+    }
+}
---
+++ MDMEPZ/Dto/Route/Visit/Visit.cs
@@ -0,0 +1,25 @@
+﻿using MDMEPZ.Dto.AnotherDto.Division;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MDMEPZ.Dto
+{
+    public class Visit
+    {
+        public bool OBJECT { get; set; }
+        public string NEW { get; set; }
+        public bool SAVE { get; set; }
+        public bool Предопределенный { get; set; }
+        public LinkVisit Ссылка { get; set; }
+        public bool ПометкаУдаления { get; set; }
+        public OwnerVisit Владелец { get; set; }
+        public string Наименование { get; set; }
+        public string Код { get; set; }
+        public string НаименованиеКраткое { get; set; }
+        public ReferenceAnotherDevision Подразделение { get; set; }
+        public Region Участок { get; set; }
+    }
+}

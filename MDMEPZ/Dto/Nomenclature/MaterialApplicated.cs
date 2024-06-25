64763b0480dec8e4858d577570c785f5119143a5
Павел Гаджиев <gadzhievpasha@gmail.com>
2024-06-25 16:59:44 +0300

    добавлен класс для материала в операции с указаниепм применяемости

---
+++ MDMEPZ/Dto/Nomenclature/MaterialApplicated.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MDMEPZ.Dto.Nomenclature
+{
+    /// <summary>
+    /// Материал с указанием применяемости
+    /// </summary>
+    public class MaterialApplicated
+    {
+        public Nomenclature nomenclature {  get; set; }
+        public Double amount {  get; set; }
+        public UnitOfMeasurement unitOfMeasurement { get; set; }
+    }
+}

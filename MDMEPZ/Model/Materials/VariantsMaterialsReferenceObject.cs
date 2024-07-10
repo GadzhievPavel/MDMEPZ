4017bee2d7798c98f020c55a67f09b5157e4a45d
Павел Гаджиев <gadzhievpasha@gmail.com>
2024-07-10 17:00:55 +0300

    edit

--- MDMEPZ/Model/Materials/VariantsMaterialsReferenceObject.cs
+++ MDMEPZ/Model/Materials/VariantsMaterialsReferenceObject.cs
@@ -1,1 +1,5 @@
-namespace TFlex.DOCs.References.VariantsMaterials{	using System;	using TFlex.DOCs.Model.References;	using TFlex.DOCs.Model.Structure;	using TFlex.DOCs.Model.References.Links;	using TFlex.DOCs.Model.Classes;	using TFlex.DOCs.Model.Parameters;			public partial class VariantsMaterialsReferenceObject : SpecialReferenceObject<VariantsMaterialsReference>	{	}}
+namespace TFlex.DOCs.References.VariantsMaterials{	using System;	using TFlex.DOCs.Model.References;	using TFlex.DOCs.Model.Structure;	using TFlex.DOCs.Model.References.Links;	using TFlex.DOCs.Model.Classes;	using TFlex.DOCs.Model.Parameters;
+    using TFlex.DOCs.Model.References.Materials;
+    using MDMEPZ.Util;
+
+    public partial class VariantsMaterialsReferenceObject : SpecialReferenceObject<VariantsMaterialsReference>	{	}}

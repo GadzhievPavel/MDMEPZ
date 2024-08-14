e9a85df13b442a8e0b0d722bccc5ef0bfaf25593
Павел Гаджиев <gadzhievpasha@gmail.com>
2024-08-14 15:10:04 +0300

    variants

--- MDMEPZ/Model/Materials/VariantsMaterialsReference.cs
+++ MDMEPZ/Model/Materials/VariantsMaterialsReference.cs
@@ -1,33 +1,54 @@
-namespace TFlex.DOCs.References.VariantsMaterials{	using System;	using TFlex.DOCs.Model.References;	using TFlex.DOCs.Model.Structure;	using TFlex.DOCs.Model.Classes;	using TFlex.DOCs.Model;
+namespace TFlex.DOCs.References.VariantsMaterials
+{
+	using System;
+	using TFlex.DOCs.Model.References;
+	using TFlex.DOCs.Model.Structure;
+	using TFlex.DOCs.Model.Classes;
+	using TFlex.DOCs.Model;
     using TFlex.DOCs.Model.References.Materials;
     using TFlex.DOCs.Model.Search;
     using System.Linq;
     using MDMEPZ.Util;
 
-    public partial class VariantsMaterialsReference : SpecialReference<VariantsMaterialsReferenceObject>	{				public partial class Factory		{		}		/// <summary>
+    public partial class VariantsMaterialsReference : SpecialReference<VariantsMaterialsReferenceObject>
+	{
+		
+		public partial class Factory
+		{
+		}
+
+		/// <summary>
 		/// Создание объекта на основе существующего материала
 		/// </summary>
 		/// <param name="source"></param>
-		/// <returns></returns>		//public ReferenceObject CreateReferenceObject(MaterialReferenceObject source)
+		/// <returns></returns>
+		//public ReferenceObject CreateReferenceObject(MaterialReferenceObject source)
 		//{
 		//	var obj = CreateReferenceObject() as VariantsMaterialsReferenceObject;
 		//	obj.StartUpdate();
 		//	obj.Name.Value = source.Name;
 		//	obj.LinkedMaterial = source;
 		//	return obj;
-		//}		/// <summary>
+		//}
+
+		/// <summary>
 		/// Создание объекта на основе существующего материала и подключение к родительскому объекту
 		/// </summary>
 		/// <param name="source"></param>
 		/// <param name="parent"></param>
-		/// <returns></returns>		public ReferenceObject CreateReferenceObject(MaterialReferenceObject source, ReferenceObject parent)
+		/// <returns></returns>
+		public ReferenceObject CreateReferenceObject(MaterialReferenceObject source, ReferenceObject parent)
 		{
 			var obj = CreateReferenceObject(parent) as VariantsMaterialsReferenceObject;
             obj.StartUpdate();
             obj.Name.Value = source.Name;
             obj.LinkedMaterial = source;
 			return obj;
-		}		public VariantsMaterialsReferenceObject FindByMaterial(ReferenceObject material)
+		}
+
+		public VariantsMaterialsReferenceObject FindByMaterial(ReferenceObject material)
 		{
 			return Find(Filter.Parse($"[Связанный материал] = '{material}'", this.ParameterGroup)).FirstOrDefault() as VariantsMaterialsReferenceObject;
-		}	}}
+		}
+	}
+}

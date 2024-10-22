59825b4c6d59022f99799206bcb3a66c17c18e37
workPC <iburych@bk.ru>
2024-10-22 09:50:08 +0300

    Add DTO Region

---
+++ MDMEPZ/Dto/AnotherDto/Region/ReferenceRegion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MDMEPZ.Dto.AnotherDto.Region
+{
+   public class ReferenceRegion
+    {
+        public string TYPE { get; set; }
+        public string UID { get; set; }
+    }
+}
---
+++ MDMEPZ/Dto/AnotherDto/Region/Region.cs
@@ -0,0 +1,18 @@
+﻿using MDMEPZ.Dto.AnotherDto.Division;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MDMEPZ.Dto.AnotherDto.Region
+{
+    public class Region
+    {
+        public ReferenceRegion Ссылка { get; set; }
+        public OwnerRegion Владелец { get; set; }
+        public string Наименование { get; set; }
+        public string Код { get; set; }
+
+    }
+}

cd98d3aedebfad6865d461e9dc35cfaf79892fde
workPC <iburych@bk.ru>
2024-10-22 09:19:50 +0300

    Add model Region

---
+++ MDMEPZ/Model/Region/RegionReference.cs
@@ -0,0 +1,1 @@
+namespace TFlex.DOCs.References.Region{	using System;	using TFlex.DOCs.Model.References;	using TFlex.DOCs.Model.Structure;	using TFlex.DOCs.Model.Classes;	using TFlex.DOCs.Model;			public partial class RegionReference : SpecialReference<RegionReferenceObject>	{				public partial class Factory		{		}	}}

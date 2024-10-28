712a61d9458a32b6d6ce7ce0db7f6225da6d61cd
workPC <iburych@bk.ru>
2024-10-28 16:18:39 +0300

    + void создание ИТРП подключения

--- MDMEPZ/Model/References1C/newConnection/ConnectionNomenclaturesReference.cs
+++ MDMEPZ/Model/References1C/newConnection/ConnectionNomenclaturesReference.cs
@@ -1,14 +1,23 @@
-namespace TFlex.DOCs.References.ConnectionNomenclatures{	using System;	using TFlex.DOCs.Model.References;	using TFlex.DOCs.Model.Structure;	using TFlex.DOCs.Model.Classes;
+namespace TFlex.DOCs.References.ConnectionNomenclatures
+{
+	using System;
+	using TFlex.DOCs.Model.References;
+	using TFlex.DOCs.Model.Structure;
+	using TFlex.DOCs.Model.Classes;
     using TFlex.DOCs.Model;
     using TFlex.DOCs.References.NomenclatureERP;
     using MDMEPZ.Dto;
     using System.Collections.Generic;
     using TFlex.DOCs.Model.Search;
     using System.Linq;
+    using MDMEPZ.Dto.ITRP;
 
-    public partial class ConnectionNomenclaturesReference : SpecialReference<ConnectionNomenclaturesReferenceObject>	{
+    public partial class ConnectionNomenclaturesReference : SpecialReference<ConnectionNomenclaturesReferenceObject>
+	{
         private NomenclatureMDMReference nomenclatureReference;
-        public partial class Factory		{		}
+        public partial class Factory
+		{
+		}
 
         private void loadReference()
         {
@@ -26,9 +35,21 @@ namespace TFlex.DOCs.References.ConnectionNomenclatures{	using System;	using 
             return connectionRefObj;
         }
 
+        public ReferenceObject CreateReferenceObjectITRP(ConnectNumenclatureITRP connection)
+        {
+            var connectionRefObj = CreateReferenceObject(Classes.ConnectionNomenclaturesReferenceObjectITRP) as ConnectionNomenclaturesReferenceObject;
+            connectionRefObj.Amount.Value = connection.Количество;
+            connectionRefObj.CurrentParam.Value = connection.Изделие;
+            connectionRefObj.ChildParam.Value = connection.Деталь;
+            connectionRefObj.Child = nomenclatureReference.findObjectByGuid1C(connection.Деталь);
+            connectionRefObj.Current = nomenclatureReference.findObjectByGuid1C(connection.Изделие);
+            return connectionRefObj;
+        }
+
         public List<ConnectionNomenclaturesReferenceObject> findConnectionByParentNomenclature(NomenclatureMDMReferenceObject nomErp)
         {
             var filter = Filter.Parse($"[родительский объект] = '{nomErp.GUID1C}'", ParameterGroup);
             return Find(filter).Cast<ConnectionNomenclaturesReferenceObject>().ToList();
         }
-    }}
+    }
+}
